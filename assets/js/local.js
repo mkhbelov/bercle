be6b5afa925d80525707bcf40385b2145af30eb9
sharpeenko <web_eff@mail.ru>
2015-08-19 19:19:56 +0300

    Начало разработки (header, footer, site)

---
+++ assets/js/local.js
@@ -0,0 +1,22 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$(document).ready(function ( ) {
+    if ($('#site').length > 0) {
+        var PageHeight = $(window).height();
+        var HeaderHeight = $('#header').height();
+        var FooterHeight = $('#footer').height();
+        var SiteHeight = PageHeight - HeaderHeight - FooterHeight;
+        $('#site').css('min-height', SiteHeight);
+    }
+
+   
+
+
+
+});
+
+
